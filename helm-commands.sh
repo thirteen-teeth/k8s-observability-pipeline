9615e991f1d3012723b7998652ee97e1d97d3c11
emorris <emorris2@outlook.com>
2023-11-01 22:05:51 -0400

    changes

--- helm-commands.sh
+++ helm-commands.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -ueo pipefail
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add jetstack https://charts.jetstack.io
 helm repo add jaegertracing https://jaegertracing.github.io/helm-charts
